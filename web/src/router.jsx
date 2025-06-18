df3770e1d1d32023ed5000c736b8a9905688242e
YuqiLB <bondliuyq@gmail.com>
2025-06-18 14:30:14 -0600

    Update router.jsx

--- web/src/router.jsx
+++ web/src/router.jsx
@@ -1,11 +1,9 @@
 import Index from './pages/index.jsx'
-import Navbar from './navbar/NavBar.jsx';
 import { createBrowserRouter, createRoutesFromElements, Route} from "react-router-dom";
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
     <Route path="/" element={<Index />} />
-    <Route path="/navbar" element={<Navbar />} />
     </>
   )
 );
