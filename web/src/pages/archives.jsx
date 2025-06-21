d23d827849622ea4d7e578cabbfbaa2f76c78741
YuqiLB <bondliuyq@gmail.com>
2025-06-20 20:52:30 -0600

    Make more pages

---
+++ web/src/pages/archives.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ArchivesPage = () => {
+  return (
+    <div className="archives-page">
+      <h1>Story Archives</h1>
+      <p>Here you’ll find a collection of stories we've preserved.</p>
+      {/* Add your archive content here */}
+    </div>
+  );
+};
+
+export default ArchivesPage;
